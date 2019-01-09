04d8e34a46bc1d7e35dc72edc76f423666451ef8
albo-ai <borji@uwm.edu>
2019-01-08 19:43:43 -0800

    ddd

--- tt.py
+++ tt.py
@@ -1,1 +1,30 @@
+# https://help.github.com/articles/updating-credentials-from-the-osx-keychain/
+
+# How to solve the problem when github gives errors for denying acess specifcally to this error:
+#    remote: Permission to albo - ai / testg.git denied to aliborji. fatal: unable to
+#     access
+#     'https://github.com/albo-ai/testg.git/': The
+#     requested
+#     URL
+#     returned
+#     error: 403
+
+# solution: I updated the username and password in keychain in mac
+# see also https://help.github.com/articles/updating-credentials-from-the-osx-keychain/
+
+
+# …or create a new repository on the command line
+# echo "# testg" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git remote add origin https://github.com/albo-ai/testg.git
+# git push -u origin master
+# …or push an existing repository from the command line
+# git remote add origin https://github.com/albo-ai/testg.git
+# git push -u origin master
+
+
+
 print('hi')
+print('hi again!')
